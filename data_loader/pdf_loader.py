b864634271f729a8f7d4e44cd2aaa231f4581d08
Conor Schweppe <conorschweppe@Conors-MacBook-Pro.local>
2025-07-19 00:25:37 +0100

    Updated PDF Loader

--- data_loader/pdf_loader.py
+++ data_loader/pdf_loader.py
@@ -2,11 +2,3 @@
 
 import fitz
 
-def load_pdf_to_string('\Users\conorschweppe\Desktop\Downloaded_Notes\Calculus_Notes\23MS244R.pdf'):
-open(pdf_path, 'rb') as file:
-    pdf_document = fitz.open(file)
-    text = ""
-    for page_num in range(len(pdf_document)):
-        page = pdf_document[page_num]
-        text += page.get_text()
-    return text
